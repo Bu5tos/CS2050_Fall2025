17d2b8e427ea61d61fb0ad723def2943f715b741
Bu5tos <alexis.bustos2006@icloud.com>
2025-09-01 19:40:20 -0600

    2D Arrays

---
+++ CS2050ClassWork/PracticeClass.java
@@ -0,0 +1,28 @@
+//import java.util.Scanner;
+
+public class PracticeClass {
+
+	public static void main(String[] args) {
+		
+		//Constant variables to be absolute
+		final int TABLE_ROW_SIZE = 5;
+		final int TABLE_COL_SIZE = 6;
+		
+		//Declaring a 2D Array named table
+		int[][] table = new int [TABLE_ROW_SIZE][TABLE_COL_SIZE];
+		
+		/*Nested for loop
+		 *Outer loop goes through each row
+		 *Inner loop goes through each column for CURRENT row
+		 * */
+		for (int row = 0; row < TABLE_ROW_SIZE; row++) 
+		{
+		   for (int col = 0; col < TABLE_COL_SIZE; col++)
+		   {
+			   table[row][col] = (row + 1) * (col + 1);
+		   }
+		}// End loop
+		
+	}//Main End
+
+}//PracticeClass End
